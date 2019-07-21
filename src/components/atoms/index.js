78ef6569671f9968ef93c4c7686010bd48b7c95f
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-07-21 22:52:04 +0200

    remove require.context

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,3 +1,4 @@
+/*
 const cache = {};
 const req = require.context('./', false, /\.(js|tsx)$/);
 
@@ -6,3 +7,28 @@ req.keys().forEach(filename => {
 });
 
 export default cache;
+*/
+
+import AppBar from "./AppBar";
+import Avatar from "./Avatar";
+import Badge from "./Badge";
+import Button from "./Button";
+import Chip from "./Chip";
+import Divider from "./Divider";
+import Icon from "./Icon";
+import IconButton from "./IconButton";
+import Toolbar from "./Toolbar";
+import Typography from "./Typography";
+
+export default {
+  AppBar,
+  Avatar,
+  Badge,
+  Button,
+  Chip,
+  Divider,
+  Icon,
+  IconButton,
+  Toolbar,
+  Typography
+};
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
+/*
 const cache = {};
 const req = require.context('./', false, /\.(js|tsx)$/);
 
@@ -6,3 +7,34 @@ req.keys().forEach(filename => {
 });
 
 export default cache;
+*/
+
+import Card from "./Card";
+import CardActionArea from "./CardActionArea";
+import CardActions from "./CardActions";
+import CardContent from "./CardContent";
+import CardMedia from "./CardMedia";
+import Drawer from "./Drawer";
+import InputAdornment from "./InputAdornment";
+import List from "./List";
+import ListItem from "./ListItem";
+import ListItemIcon from "./ListItemIcon";
+import ListItemText from "./ListItemText";
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
+export default {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Drawer,
+  InputAdornment,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Tab,
+  Tabs
+};
